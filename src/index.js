b42b1a80c540c5e3994389e0232fc2c128238377
Derrick Bray <derrickbray@yahoo.com>
2017-07-16 00:26:35 -0500

    add html element and innertext

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import style from './style.css';
 var name = require('./name');
 
 console.log('Hello my name is ' + name.name);
+
+var hello = document.getElementsByClassName('hello');
+hello[0].innerText = 'hey my name is ' + name.name;
